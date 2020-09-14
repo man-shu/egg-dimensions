eb1aa8ce3e3bd3b3658d66657d036efbd2cb227b
himanshu <himanshu@ubuntu-20.04>
2020-09-15 00:34:20 +0530

    rounded dilation and tilted rectangles; TODO fix overlapping rectangles

--- measure-eggs.py
+++ measure-eggs.py
@@ -3,11 +3,11 @@ from scipy.spatial import distance as dist
 from imutils import perspective
 from imutils import contours
 import numpy as np
-import argparse
 import imutils
 import cv2
 import matplotlib.pyplot as plt
 from skimage.morphology import medial_axis
+from skimage.morphology import skeletonize
 import os
 import csv
 
@@ -16,29 +16,24 @@ def get_contour_precedence(contour, cols):
     origin = cv2.boundingRect(contour)
     return ((origin[1] // tolerance_factor) * tolerance_factor) * cols + origin[0]
 
-# Python program to check if rectangles overlap
-class Point:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-  
-# Returns true if two rectangles(l1, r1)  
-# and (l2, r2) overlap 
-def doOverlap(l1, r1, l2, r2):
+def rectArea(x):
+	return x[0]*x[1]
 
-    # If one rectangle is on left side of other 
-	if(l1.x > r2.x or l2.x > r1.x):
-		return False
-  
-    # If one rectangle is above other 
-	if(l1.y < r2.y or l2.y < r1.y):
-		return False
-  
-	return True
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-def rectArea(w,h):
-	return w*h
+def create_circular_mask(h, w, center=None, radius=None):
 
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = (dist_from_center <= radius).astype(np.uint8)
+    return mask
 
 directory = './toMeasure/'
 files = [f for f in os.listdir(directory) if (os.path.isfile(os.path.join(directory, f)) and f.split('.')[-1]=='jpg')]
@@ -85,19 +80,16 @@ for filename in files:
 	cv2.waitKey(0)
 
 	contours.sort(key=lambda x:get_contour_precedence(x, image.shape[1]))
-	
 	contours = [cnt for cnt in contours if cv2.contourArea(cnt)>2500]
 	ref_contour = contours[0]
-	print(cv2.contourArea(ref_contour))
 	(ref_x,ref_y,ref_w,ref_h) = cv2.boundingRect(ref_contour)
-
 	cv2.rectangle(image,(ref_x,ref_y),
 				(ref_x+ref_w,ref_y+ref_h),
 				(0,255,0),1)
-
 	scale = (float(scale_length)/ref_w)
 
-	edged = cv2.dilate(edged, None, iterations=15)
+	kernel = create_circular_mask(38,38)
+	edged = cv2.dilate(edged, kernel, iterations=1)
 
 	contours, hier = cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -108,70 +100,66 @@ for filename in files:
 	cv2.imshow('image',edged)
 	cv2.waitKey(0)
 
+	# edged = np.int0(edged/255)
 	edged = (medial_axis(edged).astype(np.uint8))*255
 
 	cv2.imshow('image',edged)
 	cv2.waitKey(0)
 
 	contours, hier = cv2.findContours(edged,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	contours = [cnt for cnt in contours if cv2.contourArea(cnt)>1000]
-	rects = [cv2.boundingRect(cnt) for cnt in contours]
 
-	len_cnts = len(contours)
+	contours = [cnt for cnt in contours if cv2.contourArea(cnt)>6000]
 
-	rects_toKeep = []
+	tilted_rects = [cv2.minAreaRect(cnt) for cnt in contours]
+	# print(tilted_rects)
+	dims = [rect[1] for rect in tilted_rects]
+	boxes = [np.int0(cv2.boxPoints(rect)) for rect in tilted_rects]
 
-	for rect1 in range(0,len_cnts):
-		(x1,y1,w1,h1) = rects[rect1]
-		l1 = Point(x1,y1)
-		r1 = Point(x1+w1,y1-h1)
-		count_overlaps = []
-		for rect2 in range(0,len_cnts):
-			(x2,y2,w2,h2) = rects[rect2]
-			l2 = Point(x2,y2)
-			r2 = Point(x2+w2,y2-h2)
-			if(doOverlap(l1, r1, l2, r2)):
-				if rectArea(w1,h1)>rectArea(w2,h2):
-					count_overlaps.append(rect2)
-					if len(count_overlaps)>2:
-						areas = []
-						for i in count_overlaps:
-							(x2,y2,w2,h2) = rects[i]
-							area_overlap = areas.append(abs(rectArea(w1,h1)-rectArea(w2,h2)))
-						rect2 = count_overlaps[areas.index(max(areas))]
-					rects_toKeep.append(rects[rect2])
-				else:
-					continue
-			else:
-				continue
-
-	rects_toKeep = list(set(rects_toKeep))
+	boxes = sorted(boxes, key=cv2.contourArea)
+	dims = sorted(dims, key=rectArea)
+	len_boxes = len(boxes)
 
 	with open('eggs_dimensions.csv', 'a') as csvfile:
 		filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 		filewriter.writerow([filename,scale_length])
-
-		if len(rects_toKeep) == 0:
+		if len_boxes == 0:
 			print("Couldn't find any eggs :(")
 			filewriter.writerow([None, None])
 		else:
-			for rect in rects_toKeep:
+			# if len_boxes%2 == 0:
+			# 	boxes = boxes[0:int(len_boxes/2)]
+			# 	dims = dims[0:int(len_boxes/2)]
+			# else:
+			# 	boxes = boxes[0:int(len_boxes/2+1)]
+			# 	dims = dims[0:int(len_boxes/2+1)]
 
-				cv2.rectangle(image,(rect[0],rect[1]),
-						(rect[0]+rect[2],rect[1]+rect[3]),
-						(0,0,255),1)
+			for dim in dims:
+				i = dims.index(dim)
 
-				i = rects_toKeep.index(rect)
-				width = '{}mm'.format(str(rect[2]*scale)[:6])
-				height = '{}mm'.format(str(rect[3]*scale)[:6])
+				cv2.drawContours(image,[boxes[i]],0,(0,0,255),1)
+
+				width = '{}mm'.format(str(dim[0]*scale)[:6])
+				height = '{}mm'.format(str(dim[1]*scale)[:6])
+				
+				if width>height:
+					temp_w = height
+					height = width
+					width = temp_w
+
+				box = perspective.order_points(boxes[i])
+				(tl, tr, br, bl) = box
+				(tltrX, tltrY) = midpoint(tl, tr)
+				(tlblX, tlblY) = midpoint(tl, bl)
+
+				cv2.putText(image, '{}-{}'.format(i,width),
+					(int(tltrX-30), int(tltrY)), cv2.FONT_HERSHEY_SIMPLEX,
+					0.45, (0, 255, 0), 1)
+				cv2.putText(image, '{}-{}'.format(i,height),
+					(int(tlblX), int(tlblY)), cv2.FONT_HERSHEY_SIMPLEX,
+					0.45, (0, 0, 255), 1)
 
 				filewriter.writerow([i,width, height])
-
-				cv2.putText(image, '{}-{}mm,{}mm'.format(i,str(rect[2]*scale)[:5],str(rect[3]*scale)[:5]),
-					(int(rect[0]-10), int(rect[1]-10)), cv2.FONT_HERSHEY_SIMPLEX,
-					0.45, (119, 119, 119), 1)
-			
 				print('{}-{},{}'.format(i,width,height))
 
 	cv2.imwrite('./Measured/{}'.format(filename), image) 
